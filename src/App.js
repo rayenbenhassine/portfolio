503531d7dfea626cacf5b39fcbb1246975c223d5
rayen <rayenbenhassine7@gmail.com>
2023-03-16 18:20:29 +0100

    rayene's portforlio

--- src/App.js
+++ src/App.js
@@ -35,8 +35,9 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
+          <Route path="/" element={<Navigate to="/portfolio" />} />
+          <Route path="/portfolio" element={<Home />} />
+          <Route path="/project" element={<Projects />} /> 
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/"/>} />
